2de878941528dd0df3c131ab088a9e5bc4c09b1d
Sarah <sarahraqueld@gmail.com>
2017-10-08 16:16:53 -0300

    Add events to the project's apps

--- integra_ti/integra_ti/settings.py
+++ integra_ti/integra_ti/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PART_APPS = [
+    'rest_framework',
+]
+LOCAL_APPS = [
+    'events',
+]
+
+INSTALLED_APPS = LOCAL_APPS + THIRD_PART_APPS + DEFAULT_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
