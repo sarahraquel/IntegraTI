76cde1ffe14e2b68ff8c6fbcbf12a88eb2e23841
Sarah <sarahraqueld@gmail.com>
2017-10-08 17:03:49 -0300

    Add event models

--- integra_ti/events/models.py
+++ integra_ti/events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class EventType(models.Model):
+    name = models.CharField(max_length=100)
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    location = models.CharField(max_length=100)
+    event_type = models.ForeignKey(EventType)
+
